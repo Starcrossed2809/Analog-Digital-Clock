282168cd431948bd3380bef85f8b8bc5b58b8f8c
StarcrossedAshu2809 <yadavashutoshkr748847@gmail.com>
2025-01-28 23:04:30 +0530

    Clock

---
+++ script.js
@@ -0,0 +1,14 @@
+// let hr = document.querySelector('#hrs');
+// let mn = document.querySelector('#min');
+// let sc = document.querySelector('#sec');
+
+// setInterval(()=>{
+//     let dat = new Date();
+//     let hh = dat.getHours() * 30;
+//     let mm = dat.getMinutes() * 6;
+//     let ss = dat.getSeconds() * 6;
+
+//     hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+//     mn.style.transform = `rotateZ(${mm}deg)`;
+//     sc.style.transform = `rotateZ(${ss}deg)`;
+//     })
